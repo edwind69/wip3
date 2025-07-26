3b2e6a98ed870d5c03d93aa94f23432eae8b26a0
edwind69 <89815576+edwind69@users.noreply.github.com>
2025-07-27 11:15:49 +1200

    third database.sql

--- database.sql
+++ database.sql
@@ -1,1 +1,1 @@
-select dbid, name, db_unique_name, database_role, open_mode, log_mode, flashback_on from v$database;
+select name, db_unique_name, database_role, open_mode, log_mode, flashback_on from v$database;
