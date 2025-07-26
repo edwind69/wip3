ec1ddf4b088cd16d30717e7381badf99f59972f4
Ed <edwinpradeep@yahoo.com>
2025-07-26 23:07:00 +0000

    second database.sql

--- database.sql
+++ database.sql
@@ -1,1 +1,1 @@
-select name, db_unique_name, database_role, open_mode, log_mode, flashback_on from v$database;
+select dbid, name, db_unique_name, database_role, open_mode, log_mode, flashback_on from v$database;
