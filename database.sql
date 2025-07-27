9aa4ed0153411982a8fc2368a0bcd6aef1ec0e5a
Ed <edwinpradeep@yahoo.com>
2025-07-27 01:00:01 +0000

    Added v statment

--- database.sql
+++ database.sql
@@ -1,1 +1,2 @@
-select name, db_unique_name, database_role, open_mode, log_mode, flashback_on from v$database;
+select dbid, name, db_unique_name, database_role, open_mode, log_mode, flashback_on from v$database;
+select host_name, instance_name from v$instance;
