e76225cac42b2a400ccdca8b2e9f1f79f272be9d
Ed <edwinpradeep@yahoo.com>
2025-07-27 01:04:58 +0000

    added dba_data_files to database.sql

--- database.sql
+++ database.sql
@@ -1,2 +1,3 @@
 select dbid, name, db_unique_name, database_role, open_mode, log_mode, flashback_on from v$database;
 select host_name, instance_name from v$instance;
+select file_name from dba_data_files;
